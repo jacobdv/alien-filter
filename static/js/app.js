a7e2a0e14341a77ba2c13476147ce4174cf1a5d0
Jacob de Vries <mrjacobdevries@gmail.com>
2021-03-29 09:33:18 -0700

    Adding filter functionality in js. Need to add on button click.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,13 @@
 // setup
 var sightings = data;
-const tbody = d3.select('tbody')
+
+// sets up button and form
+let button = d3.select("#button"),
+    form = d3.select('#form');
+
 // appending sightings data in html table
+// selects table body and appends each datapoint, row by row
+const tbody = d3.select('tbody')
 sightings.forEach((sighting) => {
     let row = tbody.append('tr');
     Object.values(sighting).forEach(value => {
@@ -9,3 +15,15 @@ sightings.forEach((sighting) => {
         cell.text(value);
     })
 });
+
+// filters
+// date
+const runFilter = () => {
+    d3.event.preventDefault();
+    let dateInput = d3.select('#datetime'), 
+        dateValue = dateInput.property('value');
+    let dateFilter = sightings.filter(s => s.datetime === dateValue);
+};
+
+button.on("click", runFilter);
+form.on("submit",runFilter);
